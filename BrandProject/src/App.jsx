c5e7478abc17694bdaa87d4f0584aa93ab7d6a69
21SW066 <zainailqk@gmail.com>
2024-11-22 08:39:24 +0500

    first commit

---
+++ BrandProject/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Navigation  from './components/Navigation';
+import HeroSecttion from './components/Hero';
+
+function App() {
+  return (
+    <div className='App'>
+      <Navigation />
+      <HeroSecttion />
+    </div>
+  )
+}
+
+export default App
---
+++ BrandProject/src/components/Hero.jsx
@@ -0,0 +1,32 @@
+
+
+function Hero() {
+  return (
+    <main className="hero container">
+        <div className="hero-content"> 
+            <h3>YOUR FEET DESIRE THE BEST</h3>
+            <p>
+                YOUR FEET DESRVE THE BEST AND WE'RE HERE TO HELP YOU WITH OUR 
+                SHOES. YOUR FEET DESRVE THE BEST AND WE'RE HERE TO HELP YOU WITH OUR 
+                SHOES 
+            </p>
+            <div className="hero-btn">
+                <button>Shop Now</button>
+                <button className="secondary-btn">Category</button>
+            </div>
+            <div className="shopping">
+                <p>Also Available On</p>
+                <div className="brand-icons">
+                    <img src="/images/flipkart.png" alt="amazon-logo" />
+                    <img src="/images/amazon.png" alt="amazon-logo" />
+                </div>
+            </div>
+        </div>
+        <div className="hero-image"> 
+            <img src="/images/shoes.png" alt="shoes-pic" />
+        </div>
+    </main>
+  )
+}
+
+export default Hero
